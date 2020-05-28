cf48d2a7a2d9edbab0cd1115c1603f18e9cb3bda
zbeucler2018 <49871927+zbeucler2018@users.noreply.github.com>
2020-05-28 15:04:28 -0400

    ERC20 Test Program

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var userAddress;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let ethereum = window.ethereum;
+console.log("eth: " + ethereum);
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            account: null
+        }
+    }
+
+    componentDidMount() {
+        this.getAccountName();
+    }
+
+    async getAccountName() {
+        // eslint-disable-next-line no-undef
+        this.setState({account: await ethereum.enable()}, () => {
+            userAddress = this.state.account[0];
+            var x = typeof(userAddress);
+            console.log("User Address type: " + x);
+        });
+    }
+
+    sendAPIRequest(){
+        window.alert('Transfered one token');
+        var APIAdress;
+        APIAdress = "http://13.56.163.182:8000/transfer-token";
+        axios.post(APIAdress, {
+            ticker: "BEAR",
+            amount: 1,
+            to: userAddress,
+            hookUrl: "done",
+        })
+        .then(function(response){
+            console.log("Response: " + response);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+    }
+
+
+
+    render() {
+        return <div className = "container">
+
+            <h1 className="title"> ERC20 Demo React App </h1>
+            <h3 className="title"> Click to get a BEAR token </h3>
+
+
+            <div className="img-div">
+                <img className="seaFloor" alt="The sea floor" src="https://images.unsplash.com/photo-1567001847230-ed5da95bd055?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+            </div>
+
+            <br></br>
+
+            <div className="button-div">
+                <button className="metaMask" onClick={this.sendAPIRequest}>Get BEAR Token</button>
+            </div>
+
+        </div>
+            
+    }
+}
+ReactDOM.render(<App />, document.querySelector('#root'));
+
